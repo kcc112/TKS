46538537bc7a6e1a0a855237ddb8216a9716d351
216749 Krystian Domdalski <216749@edu.p.lodz.pl>
2020-03-28 18:59:38 +0100

    Added ports implementation to the Adapter class.

--- ApplicationPorts/src/main/java/hex/adapters/TechnologyRepositoryAdapter.java
+++ ApplicationPorts/src/main/java/hex/adapters/TechnologyRepositoryAdapter.java
@@ -2,6 +2,9 @@ package hex.adapters;
 
 
 import hex.adapters.ports.IGetAllTechnologies;
+import hex.adapters.ports.IGetAllTechnologiesBackEnd;
+import hex.adapters.ports.IGetAllTechnologiesFrontEnd;
+import hex.adapters.ports.ISelectTechnologyByName;
 import hex.model.technologies.Technology;
 import hex.model.technologies.TechnologyEnt;
 import hex.repository.interfaces.ITechnologyRepositoryEnt;
@@ -10,11 +13,13 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static hex.adapters.converter.ToDomainConverterTechnology.convertTechnology;
 
 @Component
-public class TechnologyRepositoryAdapter implements IGetAllTechnologies<Technology> {
+public class TechnologyRepositoryAdapter implements IGetAllTechnologies<Technology>,
+        IGetAllTechnologiesBackEnd<Technology>, IGetAllTechnologiesFrontEnd<Technology>, ISelectTechnologyByName<Technology> {
 
     private final ITechnologyRepositoryEnt technologyRepositoryEnt;
 
@@ -26,10 +31,36 @@ public class TechnologyRepositoryAdapter implements IGetAllTechnologies<Technolo
     @Override
     public List<Technology> getAllTechnologies() {
         List<Technology> technologies = new ArrayList<>();
-        for(TechnologyEnt technologyEnt : technologyRepositoryEnt.getAllTechnologies()) {
+        for (TechnologyEnt technologyEnt : technologyRepositoryEnt.getAllTechnologies()) {
             Technology technology = convertTechnology(technologyEnt);
             technologies.add(technology);
         }
         return technologies;
+    }
+
+    @Override
+    public List<Technology> getAllTechnologiesBackEnd() {
+        List<Technology> backendTechnologies = new ArrayList<>();
+        for (TechnologyEnt technologyEnt : technologyRepositoryEnt.getAllTechnologiesBackEnd()) {
+            Technology technology = convertTechnology(technologyEnt);
+            backendTechnologies.add(technology);
+        }
+        return backendTechnologies;
+    }
+
+    @Override
+    public List<Technology> getAllTechnologiesFrontEnd() {
+        List<Technology> frontendTechnologies = new ArrayList<>();
+        for (TechnologyEnt technologyEnt : technologyRepositoryEnt.getAllTechnologiesFrontEnd()) {
+            Technology technology = convertTechnology(technologyEnt);
+            frontendTechnologies.add(technology);
+        }
+        return frontendTechnologies;
+    }
+
+    @Override
+    public Optional<Technology> selectTechnologyByName(String technologyName) {
+        TechnologyEnt technologyEnt = technologyRepositoryEnt.selectTechnologyByName(technologyName).get();
+        return Optional.of(convertTechnology(technologyEnt));
     }
 }
